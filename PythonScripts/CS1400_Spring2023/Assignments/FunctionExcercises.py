5fa2e2f440525c9af403a58ab996cfa7c6a3738d
Zac Peterson <Peterson.Zac17@gmail.com>
2023-03-02 00:19:54 -0700

    Finished function exercises

--- PythonScripts/CS1400_Spring2023/Assignments/FunctionExcercises.py
+++ PythonScripts/CS1400_Spring2023/Assignments/FunctionExcercises.py
@@ -2,14 +2,13 @@ def avg(n1, n2):
     """Return average of two numbers
     Return a message is a non-number is passed"""
     try:
-      return(n1 + n2) / 2
+        return (n1 + n2) / 2
     except TypeError:
-      return("Please use two numbers as parameters")
+        return "Please use two numbers as parameters"
 
 
 def odds_or_evens(my_bool, nums):
-    """Returns all of the odd or
-    even numbers from a list"""
+    """Returns all the odd or even numbers from a list"""
     return_list = []
     for num in nums:
         if my_bool:
@@ -20,3 +19,44 @@ def odds_or_evens(my_bool, nums):
                 return_list.append(num)
 
     return return_list
+
+
+def search_list(my_list, search_word):
+    """Search for item in a list
+    Return: index if found, -1 if not"""
+    for word in my_list:
+        if word.lower() == search_word.lower():
+            return my_list.index(word)
+    return -1
+
+
+import csv
+
+mlb_data = "student_folder/.exercises/mlb_data.csv"
+
+
+def best_team(file):
+    """Read a CSV of baseball data.
+    Return: team name with the most wins"""
+    with open(file, "r") as input_file:
+        most_wins = 0
+        team_name = ""
+        reader = csv.reader(input_file)
+        next(reader)
+        for Tm, Lg, G, W, L in reader:
+            if int(W) > most_wins:
+                most_wins = int(W)
+                team_name = Tm
+    return team_name
+
+
+def is_palindrome(word):
+    """
+    inputted word is checked if it is a palindrome
+    (letters of the word are the same forward and reverse)
+    splitting = [start:end:step] (-1 reverses)(::# does the full list)
+    Returns: true or false if it is/is not a palindrome
+    """
+    if word[::-1] == word:
+        return True
+    return False
---
+++ PythonScripts/WinDirectoryCopyFile.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+print("Maya directory is ")
+# Prompt the user for the source directory containing Python files
+source_dir = input("Enter the path of the source directory containing Python files: ")
+# Prompt the user for the destination directory where Python files will be copied
+dest_dir = input("Enter the path of the destination directory where Python files will be copied: ")
+# Prompt the user for the destination directory where Python files will be copied
+dest_dir = input("Enter the path of the destination directory where Python files will be copied: ")
+
+# Loop through all files in the source directory
+for file_name in os.listdir(source_dir):
+    # Check if the file is a Python file
+    if file_name.endswith(".py"):
+        # Get the full path of the file
+        file_path = os.path.join(source_dir, file_name)
+        # Check if a file with the same name already exists in the destination directory
+        if os.path.exists(os.path.join(dest_dir, file_name)):
+            print(f"A file with the name {file_name} already exists in the destination directory.")
+        else:
+            # Copy the file to the destination directory
+            shutil.copy(file_path, dest_dir)
+            print(f"{file_name} has been copied to the destination directory.")
