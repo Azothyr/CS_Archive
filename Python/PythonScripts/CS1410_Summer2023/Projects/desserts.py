e94442e2565e31d1dcab13f458b79706640afb91
Zac Peterson <Peterson.Zac17@gmail.com>
2023-06-01 00:31:27 -0600

    working on dessertshop3 project

--- Python/PythonScripts/CS1410_Summer2023/Projects/desserts.py
+++ Python/PythonScripts/CS1410_Summer2023/Projects/desserts.py
@@ -1,31 +1,61 @@
-class DessertItem:
-    def __init__(self, name=""):
+from abc import ABC, abstractmethod
+
+
+class DessertItem(ABC):
+    def __init__(self, name="", tax_percent=7.25):
         self.name = str(name)
+        self.tax_percent = float(tax_percent)
+
+    @abstractmethod
+    def calculate_cost(self):
+        pass
+
+    def calculate_tax(self):
+        cost = self.calculate_cost()
+        tax = round(cost * round(self.tax_percent / 100, 5), 2)
+        return tax
 
 
 class Candy(DessertItem):
-    def __init__(self, name="", candy_weight=0.0, price_per_pound=0.0):
-        super().__init__(name)
+    def __init__(self, name="", candy_weight=0.0, price_per_pound=0.0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.candy_weight = float(candy_weight)
         self.price_per_pound = float(price_per_pound)
 
+    def calculate_cost(self):
+        cost = round(self.candy_weight * self.price_per_pound, 2)
+        return cost
+
 
 class Cookie(DessertItem):
-    def __init__(self, name="", cookie_quantity=0, price_per_dozen=0.0):
-        super().__init__(name)
+    def __init__(self, name="", cookie_quantity=0, price_per_dozen=0.0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.cookie_quantity = int(cookie_quantity)
         self.price_per_dozen = float(price_per_dozen)
 
+    def calculate_cost(self):
+        cost = round(round(self.price_per_dozen / 12, 5) * self.cookie_quantity, 2)
+        return cost
+
 
 class IceCream(DessertItem):
-    def __init__(self, name="", scoop_count=0, price_per_scoop=0.0):
-        super().__init__(name)
+    def __init__(self, name="", scoop_count=0, price_per_scoop=0.0, tax_percent=7.25):
+        super().__init__(name, tax_percent)
         self.scoop_count = int(scoop_count)
         self.price_per_scoop = float(price_per_scoop)
 
+    def calculate_cost(self):
+        cost = round(self.price_per_scoop * self.scoop_count, 2)
+        return cost
+
 
 class Sundae(IceCream):
-    def __init__(self, name="", scoop_count=0, price_per_scoop=0.0, topping_name="", topping_price=0.0):
-        super().__init__(name, scoop_count, price_per_scoop)
+    def __init__(self, name="", scoop_count=0, price_per_scoop=0.0,
+                 topping_name="", topping_price=0.0, tax_percent=7.25):
+        super().__init__(name, scoop_count, price_per_scoop, tax_percent)
         self.topping_name = str(topping_name)
         self.topping_price = float(topping_price)
+
+    def calculate_cost(self):
+        cost = round(self.price_per_scoop * self.scoop_count + self.topping_price, 2)
+        return cost
--- Python/PythonScripts/CS1410_Summer2023/Projects/dessertshop.py
+++ Python/PythonScripts/CS1410_Summer2023/Projects/dessertshop.py
@@ -1,12 +1,10 @@
+import pathlib
+
 from desserts import *
+from receipt import make_receipt
 
 
 class Order:
-    """
-    order: list of DessertItem objects
-    A constructor that creates an empty Order
-    add() method that takes a single DessertItem argument and adds it to the order list
-    """
     def __init__(self, order_list=[]):
         self.order_list = order_list
 
@@ -24,6 +22,18 @@ class Order:
             raise ValueError("Error: Item to add is not a DessertItem")
         self.order_list.append(dessert_item)
 
+    def order_cost(self):
+        total_cost = 0.00
+        for item in self.order_list:
+            total_cost += item.calculate_cost()
+        return total_cost
+
+    def order_tax(self):
+        total_tax = 0
+        for item in self.order_list:
+            total_tax += item.calculate_tax()
+        return total_tax
+
 
 def main():
     """
@@ -35,14 +45,25 @@ def main():
                   IceCream("Pistachio", 2, .79),
                   Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29),
                   Cookie("Oatmeal Raisin", 2, 3.45)]
+    receipt_list = [["Name", "Item Cost", "Tax"]]
 
     my_order = Order()
     for order in order_list:
         my_order.add(order)
+    cost_subtotal = 0
+    tax_subtotal = 0
     for item in my_order:
-        print(item.name)
-    length = len(my_order)
-    print(f"Total number of items in order: {length}")
+        cost = item.calculate_cost()
+        tax = item.calculate_tax()
+        cost_subtotal += cost
+        tax_subtotal += tax
+        receipt_list.append([item.name, "${:.2f}".format(cost), "${:.2f}".format(tax)])
+    receipt_list.append(["--------------------------------------------------------", "", ""])
+    receipt_list.append(["Order Subtotals", "${:.2f}".format(cost_subtotal), "${:.2f}".format(tax_subtotal)])
+    receipt_list.append(["Order Total", "", "${:.2f}".format(cost_subtotal + tax_subtotal)])
+    receipt_list.append(["Total items in the order", "", len(my_order)])
+
+    make_receipt(receipt_list, "receipt.pdf")
 
 
 if __name__ == "__main__":
