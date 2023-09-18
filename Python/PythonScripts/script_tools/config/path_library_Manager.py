ca67b240f452a36f9cc193984d40fc5b185a6e66
Zac Peterson <Peterson.Zac17@gmail.com>
2023-09-18 17:21:40 -0600

    "

---
+++ Python/PythonScripts/script_tools/config/path_library_Manager.py
@@ -0,0 +1,55 @@
+from script_tools.utils.format_ops import dict_ops as dict_formatter
+from script_tools.components.json_config_manager_base import JsonConfigManager
+from script_tools.config.env_handler import EnvHandler
+# from script_tools.handlers.debug_handler import get_debugger
+from script_tools.components.decorators import singleton
+
+
+@singleton
+class PathManager(JsonConfigManager):
+    @staticmethod
+    def __get_environment():
+        # __debugger = get_debugger(__name__)
+        env_handler = EnvHandler()
+        print(env_handler.get_value('python_repo_base'))
+        if env_handler.get_value('config_paths.path_library_config') is None:
+            config_path = env_handler.get_value('python_repo_base') + '\\config\\_settings\\_path_library.json'
+            print(config_path)
+            env_handler.set_value('config_paths.path_library_config', config_path)
+        if env_handler.get_value('config_paths.python_repo_cache') is None:
+            cache_path = env_handler.get_value('python_repo_base') + '\\config\\_settings\\_configs_cache.json'
+            print(cache_path)
+            env_handler.set_value('config_paths.path_library_cache', cache_path)
+        return env_handler
+
+    def __init__(self, **kwargs):
+        self.__env_handler = self.__get_environment()
+        print(self.__env_handler.get_value('config_paths.path_library_config'))
+        super().__init__(json_path=self.__env_handler.get_value('config_paths.path_library_config'),
+                         cache_path=self.__env_handler.get_value('config_paths.python_repo_cache')),
+        self.__kwargs = kwargs
+        self.__library = self.config_json
+
+    def __repr__(self) -> str: return dict_formatter.format_dict_to_print(self.__library)
+
+    @property
+    def get_library(self) -> str: return self.__library
+
+    @property
+    def get_library_path(self) -> str: return self.__env_handler.get_value('config_paths.path_library_config')
+
+    @staticmethod
+    def update_library():
+        """Returns the path map"""
+        # self.__debugger.print('get path map-start')
+        # get path-1.(~) Set the paths dictionary
+        custom_scripts_paths_return = {}
+
+        # wait = True
+        # while wait:
+        #     wait = _check_for_change(run=0)
+
+
+if __name__ == "__main__":
+    path_manager = PathManager()
+    print(path_manager)
--- Python/PythonScripts/script_tools/custom_tool_inserter.py
+++ Python/PythonScripts/script_tools/custom_tool_inserter.py
@@ -8,6 +8,7 @@ Get all .py _files in the directory and subdirectories this script is run from
 Return: Writes Maya userSetup.py and places all custom scripts in Maya directory
 Set a sys env variable "pythonpath" with script folder path value.
 """
+<<<<<<< STABLE
 import platform
 try:
     from script_tools.functions.file_tools import transfer_py_dir_in_current, get_file_path_from_lib
@@ -41,3 +42,47 @@ if __name__ == "__main__":
             debugger.print('debug-3')
             debugger.print('debug-4')
             exit()
+=======
+from handlers.debug_handler import get_debugger
+from script_tools.utils.platform_ops import platform_search_ops as platform_search
+from script_tools.utils.file_ops.file_path_ops import get_file_path_from_lib as get_path
+from script_tools.utils.file_ops.file_transfer_ops import transfer_current_file_directory as transfer_py_dir
+
+# __debugger = get_debugger(__name__, _all=True, global_switch=True, trace_switch=True, check_stack=True)
+__debugger = get_debugger(__name__, global_switch=True)
+
+
+def debug_info() -> dict:
+    return {
+        "dunder main enter": "Running custom_tool_inserter.py 'dunder main'",
+        "dunder main exit": "Exiting 'dunder main'/custom_tool_inserter.py'",
+        "main-1.1": "Start of custom_tool_inserter.py 'main()'",
+        "main-2.1": "Getting paths",
+        "main-2.2": "RETURNED: repo:--{}, scripts_folder:--{}",
+        "main-2.3": "No repo found, Exiting",
+        "main-3.1": "Main of custom_tool_inserter.py finished",
+    }
+
+
+def main():
+    __debugger.print('main-1.1', start='main-1.1 ', upper=True)
+    if platform_search.platform_check("Windows"):
+        __debugger.print('main-2.1', start='main-2.1 ', upper=True)
+        repo, scripts_folder = get_path(script_repo=True, tools=True)
+        if not repo:
+            __debugger.print('main-2.3', start='main-2.3 ', upper=True)
+            exit()
+        __debugger.print('main-2.2', start='main-2.2 ', upper=True)
+
+        _exceptions = ["custom_tool_inserter.py"]
+        _file_type_inclusion = [".py", ".json", ".txt", ".md"]
+        print('conf inserter-------------------------------' + repo, scripts_folder)
+        transfer_py_dir(repo, scripts_folder, _file_type_inclusion, _exceptions)
+        __debugger.print('main-3.1', start='main-3.1', upper=True)
+
+
+if __name__ == "__main__":
+    __debugger.print('dunder main enter', start='dunder main', upper=True)
+    main()
+    __debugger.print('dunder main exit', start='main-3.2', upper=True)
+>>>>>>> "
