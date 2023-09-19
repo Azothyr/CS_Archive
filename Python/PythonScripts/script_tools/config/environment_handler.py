e3e4dcad6bbe16aef647978967315e36ca7c251a
Zac Peterson <Peterson.Zac17@gmail.com>
2023-09-19 15:41:43 -0600

    missed commits

--- Python/PythonScripts/script_tools/config/environment_handler.py
+++ Python/PythonScripts/script_tools/config/environment_handler.py
@@ -5,14 +5,15 @@ from script_tools.components.json_config_manager_base import JSONConfigManager
 
 
 def _initial_info():
-    _src = {
-        "name": "DEFAULT_ENV",
-        "comp1": ["C:/GitRepos/Scripts_Private/Python/PythonScripts/script_tools", "C:/GitRepos/MayaPythonToolbox"],
-        "comp2": ["C:/Repos/Scripts_Private/Python/PythonScripts/script_tools", "C:/Repos/MayaPythonToolbox"],
-        "custom": "Documents/custom_scripts",
-        "arg_map": "Documents/custom_scripts/maya_scripts/config/_settings/arg_map_index.json"
-    }
-    required_keys_actions = {
+    return {
+        "_src": {
+            "name": "DEFAULT_ENV",
+            "comp1": ["C:/GitRepos/Scripts_Private/Python/PythonScripts/script_tools", "C:/GitRepos/MayaPythonToolbox"],
+            "comp2": ["C:/Repos/Scripts_Private/Python/PythonScripts/script_tools", "C:/Repos/MayaPythonToolbox"],
+            "custom": "Documents/custom_scripts",
+            "arg_map": "Documents/custom_scripts/maya_scripts/config/_settings/arg_map_index.json"
+        },
+        "required_keys_actions": {
             "user": {"prompt": "Please enter this computer profile's user name:\n>>> "},
             "custom_script_path": {"prompt": "Please enter the file path to the top folder you would like the"
                                              " custom scripts to write to: (e.g. C:/custom_scripts)\n>>> ",
@@ -24,33 +25,32 @@ def _initial_info():
             "arg_map_path": {"prompt": "Please enter the arg map path:\n>>> ",
                              "action": lambda x: Path(x)}
         }
-    return _src, required_keys_actions
+    }
 
 
-class EnvironmentHandler:
-    def __init__(self, file_path: str):
+class ConfigManager:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ConfigManager, cls).__new__(cls)
+            cls._instance.configs = {}
+        return cls._instance
+
+    def add_config(self, key, file_path):
+        # Add a new JSONConfigManager instance to the dictionary
+        self.configs[key] = JSONConfigManager(file_path)
+
+    def __init__(self, file_path, environment_name: str = "DEFAULT_ENV"):
         try:
             self.config_manager = JSONConfigManager(file_path)
         except FileNotFoundError:
-            value = self.__request_user_input('No config file found, would you like to create one? (yes/no):\n>>>')
-            match value.lower():
-                case "yes":
-                    # handle start
-                case "no":
-                    # handle stop
-                case _:
+            self.__handle_file_not_found(str(Path(file_path.resolve())))
+        self.__setup_environment(environment_name)
 
-
-        self.source_info = _initial_info()[0]
-        self.required_keys = _initial_info()[1]
-
-        # Check if repo paths exist
-        python_repo_base_path = Path(self.get('python_repo_base', None))
-        maya_repo_base_path = Path(self.get('maya_repo_base', None))
-        custom_script_path = Path(self.get('custom_script_path', None))
-
-    def get(self, key: str, default: any = None): return self.config_manager.get(key) if (self.config_manager.get(key)
-                                                                                          is not None) else default
+    def get(self, key: str, default: any = None):
+        return self.config_manager.get(key) if (self.config_manager.get(key)
+                                                is not None) else default
 
     @singledispatch
     def set(self, key: str, value: any):
@@ -65,27 +65,45 @@ class EnvironmentHandler:
         else:
             self.config_manager.set(key, value)
 
-    def update(self, key: str, value: dict): self.config_manager.set_single_nested(key, value)
+    @set.register(Path)
+    def _(self, key: str, value: Path):
+        # Special implementation for Path objects
+        self.config_manager.set(key, str(value))
+
+    def update(self, key: str, value: dict):
+        self.config_manager.set_single_nested(key, value)
 
     @staticmethod
-    def is_nested_dict(d): return any(isinstance(val, dict) for val in d.values())
+    def is_nested_dict(d):
+        return any(isinstance(val, dict) for val in d.values())
 
-    @staticmethod
-    def __request_user_input(prompt: str) -> str: return InputHandler().get_text_input(prompt, allow_empty=True)
+    def __handle_file_not_found(self, file_path: str):
+        def request_user_input(prompt: str) -> str:
+            return input(prompt).strip().lower()
 
-    self.__required_keys = self.__check_environment_info().get('required_keys').keys()
-    self.__refresh_needed = False if str(Path.home()) == self.config_json.get('user') else True
+        value = request_user_input(f'No config file found at {file_path}.\n'
+                                   f'Would you like to create one at the location?\n(yes/no)>>> ')
+        match value:
+            case "yes":
+                # Code to handle 'yes' case, like creating a new file.
+                self.config_manager.write_config({})
+            case "no":
+                # Code to handle 'no' case, like maybe exiting the program or logging an error.
+                print(f"\nRESULT: Config file was not found and will not be created.")
+            case _:
+                print(f'\nERROR: Invalid input received. Please type "yes" or "no".\n')
+                # Recursively call the method again to re-prompt the user.
+                self.__handle_file_not_found(file_path)
 
-    def __check_environment(self):
+    def __setup_environment(self, env_name: str):
         # 1. Verify environment name
-        info = self.__check_environment_info()
-        env_name = self.config_json.get('environment_name')
-        if env_name is None or self.environment_name != env_name:
-            self.set_value('src_env.environment_name', self.environment_name, force_save=True)
+        info = _initial_info()
+        if env_name is None or self.get("environment.name") != env_name:
+            self.set('environment.name', env_name)
 
         # Helper function to check if a given path exists
-        def path_exists(path: str | Path) -> bool:
-            return Path(path).resolve().exists()
+        def path_exists(_path: str | Path) -> bool:
+            return Path(_path).resolve().exists()
 
         # 2. Resolve component paths
         if path_exists(info['comp1'][0]) and path_exists(info['comp1'][1]):
@@ -113,7 +131,7 @@ class EnvironmentHandler:
         for key, details in info['required_keys'].items():
             if key == 'user':
                 new_value = Path.home()
-                self.set_value(f'src_env.{key}', str(new_value), force_save=True)
+                self.set(f'environment_paths.{key}', str(new_value))
                 continue  # skip the rest of this loop iteration
 
             # Check if 'resolved_path' is available and use it if it exists
@@ -129,7 +147,7 @@ class EnvironmentHandler:
 
             # Ensure path validity or offer creation of missing paths
             if not new_path or not new_path.exists():
-                user_response = self.__request_user_input(
+                user_response = self.__handle_file_not_found(
                     f"Value for {key} is required.\n"
                     f" The provided path {new_path} does not exist.\n"
                     "Would you like to create it? (yes/no):\n>>> "
@@ -141,3 +159,9 @@ class EnvironmentHandler:
                 else:
                     raise ValueError(
                         f"Value for {key} is required, input was {new_path} but should be a path that exists.")
+
+
+
+
+if __name__ == '__main__':
+    pass
